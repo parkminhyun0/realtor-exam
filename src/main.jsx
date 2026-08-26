401d870d94c838a6f0d55d4abb260716088f8328
BIBLE_MAP <skyhyangsu63@gmail.com>
2026-08-27 00:48:26 +0900

    feat: load public records exam keyword highlights

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
 import './typography-responsive.css'
+import './registration-law-highlights.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
